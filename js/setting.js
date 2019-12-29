66cdd56b068120b3ff8b81c1301d013854c2ae8c
kei318hijiki <33718117+kei318hijiki@users.noreply.github.com>
2019-12-29 20:02:20 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,8 +5,8 @@
 
 var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 11; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+var MaxDescription = 10; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
 
-var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShift = true; // 休止期間なら週をずらすときは、true。ずらさないなら、false。金沢の仕様は、true。
